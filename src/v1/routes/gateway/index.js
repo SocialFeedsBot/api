4f035747d756b258ffb4b7041c61bbaca26f792c
jacob <jacobpbond4@gmail.com>
2021-02-25 17:23:23 +0000

    Gateway restarting/authing

---
+++ src/v1/routes/gateway/index.js
@@ -0,0 +1,68 @@
+const Base = require('../../../structures/Route');
+const jwt = require('jsonwebtoken');
+const config = require('../../../../config.json');
+
+module.exports = class Status extends Base {
+
+  constructor() {
+    super();
+
+    this.use(this.middleware);
+    this.register('get', '/auth', this.auth.bind(this));
+    this.register('post', '/restart', this.restart.bind(this));
+  }
+
+  /**
+   * Verify the user can preform requests
+   * @param req {any} Request
+   * @param res {any} Response
+   */
+  async auth(req, res) {
+    if (!req.authInfo.auth) {
+      res.status(200).json({ auth: false });
+      return;
+    }
+    if (!config.admins.includes(req.authInfo.userID) && !req.authInfo.isBot) {
+      res.status(200).json({ auth: false });
+      return;
+    }
+    res.status(200).json({ auth: true });
+  }
+
+  /**
+   * POST the restart route.
+   * @param req {any} Request
+   * @param res {any} Response
+   */
+  async restart(req, res) {
+    if (!req.authInfo.auth) {
+      res.status(500).json({ error: 'Authentication error' });
+      return;
+    }
+
+    if (req.app.locals.gw.connected) {
+      await req.app.locals.gw.restart(req.body);
+      res.status(200).json({ success: true });
+    } else {
+      res.status(500).json({ error: 'Gateway not connected' });
+    }
+  }
+
+  middleware (req, res, next) {
+    let userID;
+    let isBot;
+    let auth;
+    try {
+      let data = jwt.verify(req.headers.authorization, config.jwtSecret, { algorithm: 'HS256' });
+      userID = data.userID;
+      isBot = !!data.bot;
+      auth = true;
+    } catch(e) {
+      auth = false;
+    }
+
+    req.authInfo = { isBot, userID, auth };
+    next();
+  }
+
+};
--- src/v1/routes/status/index.js
+++ src/v1/routes/status/index.js
@@ -6,6 +6,7 @@ module.exports = class Status extends Base {
     super();
 
     this.register('get', '/', this.get.bind(this));
+    this.register('get', '/services', this.services.bind(this));
   }
 
   /**
@@ -36,6 +37,23 @@ module.exports = class Status extends Base {
       res.status(200).json({ shards: shards.flat(), feeds: feeds[0], api: api[0] });
     } else {
       res.status(200).json({ shards: [], feeds: { uptime: 0 }, api: { uptime: 0 } });
+    }
+  }
+
+  /**
+   * GET the service list route.
+   * @param req {any} Request
+   * @param res {any} Response
+   */
+  async services(req, res) {
+    if (req.app.locals.gw.connected) {
+      const services = await req.app.locals.gw.request(
+        { name: 'gateway' },
+        'Object.values(this.connections).filter(c=>c.connected).map(a => ({ id: a.cluster ? a.cluster.id : a.id, type: a.type }))');
+
+      res.status(200).json({ services: services[0] });
+    } else {
+      res.status(200).json({ services: [] });
     }
   }
 
