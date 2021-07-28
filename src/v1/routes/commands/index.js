3115bf9f259b75169bfedfe958ce9f3a5a980ec5
jacob <jacobpbond4@gmail.com>
2021-07-28 12:55:09 +0100

    fix: auth

--- src/v1/routes/commands/index.js
+++ src/v1/routes/commands/index.js
@@ -1,5 +1,7 @@
 /* Commands Route */
 const Base = require('../../../structures/Route');
+const config = require('../../../../config');
+const jwt = require('jsonwebtoken');
 
 module.exports = class Users extends Base {
 
@@ -7,7 +9,7 @@ module.exports = class Users extends Base {
     super();
 
     this.register('get', '/', this.get.bind(this));
-    this.register('toggle', '/toggle', this.toggle.bind(this));
+    this.register('post', '/toggle', this.toggle.bind(this), this.auth.bind(this));
   }
 
   /**
@@ -25,16 +27,41 @@ module.exports = class Users extends Base {
   }
 
   async toggle(req, res) {
+    console.log(req.authInfo)
     if (!req.authInfo || (!req.authInfo.isBot && !req.authInfo.admin)) {
       res.status(403);
       return;
     }
 
-    const command = req.query.command;
-    const toggle = req.query.toggle !== 'false' ? decodeURIComponent(req.query.toggle) : 'no';
+    const command = req.body.name;
+    const toggle = req.body.disabled === true ? req.body.reason : 'no';
 
     await req.app.locals.redis.set(`commands:${command}:disabled`, toggle);
-    res.status(200);
+    res.status(200).json({ success: true });
+  }
+
+  auth(req, res, next) {
+    let userID;
+    let isBot;
+    let auth;
+    let token;
+    try {
+      let data = jwt.verify(req.headers.authorization, config.jwtSecret, { algorithm: 'HS256' });
+      userID = data.userID;
+      isBot = !!data.bot;
+      token = data.access_token;
+      auth = true;
+    } catch(e) {
+      res.status(401).json({ success: false, error: 'Not authenticated' });
+      auth = false;
+    }
+
+    if (!auth) {
+      res.status(401).json({ success: false, error: 'Unauthorised' });
+      return;
+    }
+    req.authInfo = { admin: isBot || config.admins.includes(userID), isBot, userID, accessToken: token };
+    next();
   }
 
 };
