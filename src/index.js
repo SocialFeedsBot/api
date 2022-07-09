05bc7fc60c3d756c48a475b05b9a71f0c9531250
Jacob Bond <jacobpbond4@gmail.com>
2022-07-09 15:00:04 +0100

    fix: fixes

--- src/index.js
+++ src/index.js
@@ -18,8 +18,7 @@ const app = express();
 app.use(async (req, res, next) => {
   logger.debug(`${req.method.toUpperCase()} ${req.url}`);
   if (config.prometheus && config.prometheus.use) {
-    await superagent.post(`${config.prometheus.url}/request`)
-      .set('Content-Type', 'text/plain')
+    await superagent.post(`${config.prometheus.url}/request?url=${encodeURIComponent(req.url)}`)
       .send(req.url);
   }
   next();
